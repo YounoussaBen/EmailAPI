1e94a9e8269bb323fbb5e24e2d6d625ca112d3d7
YounoussaBen <younoussaabdourhaman@gmail.com>
2024-04-02 15:47:54 +0000

    Update emails service

--- emailapi/urls.py
+++ emailapi/urls.py
@@ -16,8 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Email API",
+      default_version='v1',
+      description="Email API Description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@yourapi.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('emails.urls')), 
+    path('api/', include('sales.urls')), 
 ]
--- emails/views.py
+++ emails/views.py
@@ -1,11 +1,9 @@
-# views.py
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .services.email_sender import send_email
-
-
+from sales.views import generate_sales_summary_html
+import base64
 class SendEmailAPIView(APIView):
     def post(self, request):
         try:
@@ -16,6 +14,19 @@ class SendEmailAPIView(APIView):
             text = data.get('text', '')
             attachments = data.get('attachments', [])
 
+            # Generate HTML content for the sales summary report
+            html_content = generate_sales_summary_html()
+
+            # Convert HTML content to base64
+            html_content_base64 = base64.b64encode(html_content.encode()).decode()
+
+            # Prepare email payload with HTML content as attachment
+            attachments.append({
+                'filename': 'sales_summary_report.html',
+                'content': html_content_base64
+            })
+
+            # Send email with sales summary report as attachment
             send_email(sender, recipients, subject, text, attachments)
             
             return Response({"message": "Email sent successfully"}, status=status.HTTP_200_OK)
